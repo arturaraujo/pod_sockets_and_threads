0e88971cf5fd381ae4ef969272c9a1be623551e1
Artur Araújo <arturaraujo.b@gmail.com>
2015-12-11 18:46:24 -0300

    Changing messages between server and client

--- src/pod/Cliente.java
+++ src/pod/Cliente.java
@@ -1,29 +1,50 @@
 package pod;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
-public class Cliente {
+public class Cliente extends Thread{
+	static Socket serverSocket;
+	static Scanner keyboard = new Scanner(System.in);
+	
 	public static void main(String[] args){
 		String host = "0.0.0.0";
 		int port = 5121;
 
 		try{
 			InetAddress address = InetAddress.getByName(host);
-			Socket serverSocket = new Socket(address, port);
+			serverSocket = new Socket(address, port);
+			
+			Thread thread = new Cliente();
+			thread.start();
 
 			DataOutputStream dataOutput = new DataOutputStream(serverSocket.getOutputStream());
-			//DataInputStream dataInput = new DataInputStream(serverSocket.getInputStream());
 			
 			while(serverSocket.isConnected()){
-				String string = JOptionPane.showInputDialog("Digite a mensagem: ");
+				String string = keyboard.nextLine();
 				dataOutput.writeUTF(string);
 			}
 			serverSocket.close();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void run() {
+		DataInputStream dataInput;
+		try {
+			dataInput = new DataInputStream(serverSocket.getInputStream());
+			while(true){
+				System.out.println(dataInput.readUTF());
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 	}
 }
--- src/pod/Servidor.java
+++ src/pod/Servidor.java
@@ -1,34 +1,68 @@
 package pod;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Servidor extends Thread{
 	
 	private static String mensagem, split[];
-	private static List<Cliente> usuarios = new ArrayList<Cliente>();
+	private static Map<String, DataOutputStream> usuarios;
 	private static ServerSocket serverSocket;
+	private Socket socket;
 	
 	public static void main(String[] args) throws IOException{
-		serverSocket = new ServerSocket(5121, 5);
-		Socket socket = serverSocket.accept();
-		
-		DataInputStream dataInput = new DataInputStream(socket.getInputStream());
-		mensagem = dataInput.readUTF();
-		interpretar(mensagem);
-		serverSocket.close();
+		serverSocket = new ServerSocket(5121);
+		usuarios = new HashMap<String, DataOutputStream>();
+		while(true){//espera conexão de alguem
+			Socket socket = serverSocket.accept();
+			System.out.println("Conexão estabelecida!");
+			
+			Thread thread = new Servidor(socket);
+			thread.start();
+		}
 	}
 	
-	private static void interpretar(String comando){
+	public Servidor(Socket socket) {
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			DataInputStream dataInput = new DataInputStream(socket.getInputStream());
+			DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
+			
+			dataOutput.writeUTF("Informe seu nome: ");
+			String nome = dataInput.readUTF();
+			
+			usuarios.put(nome, dataOutput);
+			while(true){
+				if(socket.isClosed())
+					break;
+				//System.out.println("Servidor aguardando mensagem...");
+				String mensagem = dataInput.readUTF();
+				interpretar(mensagem, nome);
+			}
+			System.out.println("Conexão encerrada.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void interpretar(String mensagem, String remetente)throws IOException{
 		split = mensagem.split(" ");
 		
 		switch(split[0]){
 		case "send":
-			send();
+			if(split[1].equals("-all")){
+				send(mensagem, remetente, true);
+			}
 			break;
 		case "list":
 			System.out.println(list());
@@ -44,12 +78,22 @@ public class Servidor extends Thread{
 		}
 	}
 	
-	private static void sair(){
-		
+	private void sair() throws IOException{
+		socket.close();
 	}
 	
-	private static void send(){
-		
+	private void send(String mensagem, String remetente, boolean todos) throws IOException{
+		for(String key : usuarios.keySet()){
+			if(usuarios.get(key) != usuarios.get(remetente))
+				usuarios.get(key).writeUTF(mensagem + "  pra tudiiiin");			
+		}
+	}
+	
+	private void send() throws IOException{
+		for(String key : usuarios.keySet()){
+			if(usuarios.get(key) != this.socket.getOutputStream())
+				usuarios.get(key).writeUTF(mensagem + "  pra tudiiiin");			
+		}
 	}
 	
 	private static void rename(String novoNome){
